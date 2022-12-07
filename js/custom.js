5053521eef3dd60cabdd84092a604625cdaa1bb1
K!NG <godsonogundare25@gmail.com>
2022-12-07 08:51:01 +0000

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,41 +1,41 @@
 // ----- dropdown script -----
-window.addEventListener("resize", function () {
-    "use strict"; window.location.reload();
-});
+// window.addEventListener("resize", function () {
+//     "use strict"; window.location.reload();
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-    // make it as accordion for smaller screens
-    if (window.innerWidth > 992) {
+//     // make it as accordion for smaller screens
+//     if (window.innerWidth > 992) {
 
-        document.querySelectorAll('.navbar .nav-item').forEach(function (everyitem) {
+//         document.querySelectorAll('.navbar .nav-item').forEach(function (everyitem) {
 
-            everyitem.addEventListener('mouseover', function (e) {
+//             everyitem.addEventListener('mouseover', function (e) {
 
-                let el_link = this.querySelector('a[data-bs-toggle]');
+//                 let el_link = this.querySelector('a[data-bs-toggle]');
 
-                if (el_link != null) {
-                    let nextEl = el_link.nextElementSibling;
-                    el_link.classList.add('show');
-                    nextEl.classList.add('show');
-                }
+//                 if (el_link != null) {
+//                     let nextEl = el_link.nextElementSibling;
+//                     el_link.classList.add('show');
+//                     nextEl.classList.add('show');
+//                 }
 
-            });
-            everyitem.addEventListener('mouseleave', function (e) {
-                let el_link = this.querySelector('a[data-bs-toggle]');
+//             });
+//             everyitem.addEventListener('mouseleave', function (e) {
+//                 let el_link = this.querySelector('a[data-bs-toggle]');
 
-                if (el_link != null) {
-                    let nextEl = el_link.nextElementSibling;
-                    el_link.classList.remove('show');
-                    nextEl.classList.remove('show');
-                }
+//                 if (el_link != null) {
+//                     let nextEl = el_link.nextElementSibling;
+//                     el_link.classList.remove('show');
+//                     nextEl.classList.remove('show');
+//                 }
 
 
-            })
-        });
+//             })
+//         });
 
-    }
-});
+//     }
+// });
 // --x-- dropdown script --x--
 
 // ----- jquery function -----
